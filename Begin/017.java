f87482b957d06b64de654f85f1bce59a9116da00
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-12 22:08:55 +0500

    Create 017.java

---
+++ Begin/017.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Begin17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" A = ");
+        int a = scanner.nextInt();
+        System.out.println(" B = ");
+        int b = scanner.nextInt();
+        System.out.println(" C = ");
+        int c = scanner.nextInt();
+        int ac = Math.abs(a-c);
+        int bc = Math.abs(b-c);
+        int acbc = ac + bc;
+        System.out.println(" ac = " + ac);
+        System.out.println(" bc = " + bc);
+        System.out.println(" acbc = " + acbc);
+    }
+}
