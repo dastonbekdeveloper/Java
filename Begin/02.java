19651d91e39cc214d14abcef1411beea29cb0bfa
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-12 22:01:17 +0500

    Create 02.java

---
+++ Begin/02.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Begin2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kvadrad tomonini kiriting : ");
+        int a = scanner.nextInt();
+        int S = a*a;
+        System.out.println("S = " + S);
+    }
+}
