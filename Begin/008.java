b55fda872662e3b0994c7579bfaa38172d5be930
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-12 22:05:07 +0500

    Create 008.java

---
+++ Begin/008.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Begin8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in) ;
+        System.out.println(" a = ");
+        double a = scanner.nextDouble();
+        System.out.println(" b = ");
+        double b = scanner.nextDouble();
+        double A = (a+b)/2;
+        System.out.println("A = " + A );
+    }
+}
