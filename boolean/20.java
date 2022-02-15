85d1c6d37759dbde11c9ca85d4cd9df838ad89d2
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:38:23 +0500

    Create 20.java

---
+++ boolean/20.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class boolean20 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" 3 xonali son kiriting = ");
+        int a = scanner.nextInt();
+        int b = (int) (a%10); // birliklar xonasidagi raqam
+        int c = (int) (a%100);
+        int d = (int) (c/10); // o'nliklar xonasidagi raqam
+        int e = (int) (a/100); //yukliklar xonasidagi raqam
+        System.out.println(" Uning birliklar xonasidagi raqam : " + b);
+        System.out.println(" Uning o'nliklar xonasidagi raqam : " + d);
+        System.out.println(" Uning yuzliklar xonasidagi raqam : " + e);
+        boolean l = ( !(b==d) && !(d==e) && !(b==e));
+        System.out.println(l);
+    }
+}
