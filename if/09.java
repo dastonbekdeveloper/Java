c6562338b161cdb2c1f80bb0b2857e54ab029de4
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:49:40 +0500

    Create 09.java

---
+++ if/09.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class if9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("2 ta son kiriting : ");
+        System.out.print("a =  ");
+        int a = scanner.nextInt();
+        System.out.print("b =  ");
+        int b = scanner.nextInt();
+        if (a>=b){
+            a=a+b;
+            b=a-b;
+            a=a-b;
+            System.out.println(a + " va " + b);
+        }
+        else if (b>a){
+            System.out.println(a + " va " + b);
+        }
+    }
+}
