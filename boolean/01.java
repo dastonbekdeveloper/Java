fe4894c8cdd80ec94ff3230bf919586e70a3ba0b
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:33:09 +0500

    Create 01.java

---
+++ boolean/01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class boolean1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Musbat son kiriting : ");
+        int a = scanner.nextInt();
+        boolean b = a>0;
+        System.out.println(b);
+    }
+}
