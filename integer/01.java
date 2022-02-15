b5980e6bc401a8ef629db15a0c7c7477c21a4072
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:23:40 +0500

    Create 01.java

---
+++ integer/01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Integer1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" sm = ");
+        int a = scanner.nextInt();
+        int b = a/100;
+        System.out.println("Sof " + b + " metr buladi" );
+    }
+}
