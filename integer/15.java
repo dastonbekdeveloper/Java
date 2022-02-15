5ae5f5222595eabe1588d511fa2a94e9cf991798
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:27:52 +0500

    Create 15.java

---
+++ integer/15.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Integer15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" 3 xonali son kiriting = ");
+        int a = scanner.nextInt();
+        int b = (int) (a%10); // birliklar xonasidagi raqam
+        int c = (int) (a%100);
+        int d = (int) (c/10); // o'nliklar xonasidagi raqam
+        int e = (int) (a/100); //yukliklar xonasidagi raqam
+        System.out.println(" Uning birliklar xonasidagi raqam : " + b);
+        System.out.println(" Uning o'nliklar xonasidagi raqam : " + d);
+        System.out.println(" Uning yuzliklar xonasidagi raqam : " + e);
+        System.out.print(" O'nliklar bilan yuklikdagi raqamni o'zgartiramiz : " +  d);
+        System.out.print( e );
+        System.out.print( b);
+    }
+}
