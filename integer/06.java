c66473a7c8a7423fe02446478fdc5ea6204ed9e7
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:25:06 +0500

    Create 06.java

---
+++ integer/06.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Integer6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" 2 xonali son kiriting = ");
+        int a = scanner.nextInt();
+        int b = a/10;
+        int c = a%10;
+        System.out.println(" Uning o'nliklar xonasi : " + b);
+        System.out.print(" Uning birliklar xonasi : " + c);
+
+
+
+
+
+
+    }
+}
