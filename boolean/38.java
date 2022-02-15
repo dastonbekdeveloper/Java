82d87603eb3d48a21f1ab4eff4b1de8e0e5d3279
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:43:38 +0500

    Create 38.java

---
+++ boolean/38.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class boolean38 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("x1=");
+        int x1=input.nextInt();
+        System.out.print("y1=");
+        int y1=input.nextInt();
+        System.out.print("x2=");
+        int x2=input.nextInt();
+        System.out.print("y2=");
+        int y2=input.nextInt();
+        boolean shart = Math.abs(x1-x2)==Math.abs(y1-y2);
+        System.out.println(shart);
+    }
+}
