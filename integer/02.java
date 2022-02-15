052bfff24bbe9f27b419e75a892c6ba2dfafc4ea
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:23:55 +0500

    Create 02.java

---
+++ integer/02.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Integer2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" kg = ");
+        int a = scanner.nextInt();
+        int b = a/1000;
+        System.out.println(" Sof " + b + " tonna buladi" );
+    }
+}
