c186401d41e6cfe4c05519dca8cc0e6a93a25bd8
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:40:59 +0500

    Create 30.java

---
+++ boolean/30.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class boolean30 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Uchburchak tomonlarini kiriting : ");
+        System.out.print("a = ");
+        int a = scanner.nextInt();
+        System.out.print("b = ");
+        int b = scanner.nextInt();
+        System.out.print("c = ");
+        int c = scanner.nextInt();
+        boolean d = ( (a==b)&&(b==c)&&(c==a));
+        System.out.println(d);
+    }
+}
