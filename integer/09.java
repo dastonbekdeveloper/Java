d93530a95dfb50c32b7d905da6adfbb71b1682d5
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:26:01 +0500

    Create 09.java

---
+++ integer/09.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Integer9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" 3 xonali son kiriting = ");
+        int a = scanner.nextInt();
+        int b = a/100;
+        System.out.println(" Uning yuzliklar xonasi : " + b);
+    }
+}
