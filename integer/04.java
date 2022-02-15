8da1e9d096597e36c9aabb106c35b7215779e389
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:24:31 +0500

    Create 04.java

---
+++ integer/04.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Integer4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" A kesma uzunligi = ");
+        int a = scanner.nextInt();
+        System.out.print(" B kesma uzunligi = ");
+        int b = scanner.nextInt();
+        int c = a/b;
+        System.out.println(" Demak, A kesmada B kesmani " + c + " marta joylashtirsa bo'larkan" );
+    }
+}
