675332b75d43cd5795ae3ac6beb9d94d99adfe91
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:27:36 +0500

    Create 14.java

---
+++ integer/14.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Integer14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" 3 xonali son kiriting = ");
+        int a = scanner.nextInt();
+        int b = a%10; // birliklar xonasidagi raqam
+        int c = a%100;
+        int d = c/10; // o'nliklar xonasidagi raqam
+        int e = a/100; //yukliklar xonasidagi raqam
+        System.out.println(" Uning birliklar xonasidagi raqam : " + b);
+        System.out.println(" Uning o'nliklar xonasidagi raqam : " + d);
+        System.out.println(" Uning yuzliklar xonasidagi raqam : " + e);
+        System.out.print(" O'ngdan bittasini o'chirib chapga yozsak : " +  e);
+        System.out.print( b );
+        System.out.print( d );
+    }
+}
