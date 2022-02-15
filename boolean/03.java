d5a45ef962348a80893ad55b2d16656a39c4943b
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:33:41 +0500

    Create 03.java

---
+++ boolean/03.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class boolean3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("juft son kiriting : ");
+        int a = scanner.nextInt();
+        boolean b = a%2==0;
+        System.out.println(b);
+    }
+}
