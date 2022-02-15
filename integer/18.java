778cc35f9b19c8eb7d5f20d2918ccc783b0c6e9c
dastonbekdeveloper <79925577+dastonbekdeveloper@users.noreply.github.com>
2022-02-16 00:28:44 +0500

    Create 18.java

---
+++ integer/18.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Integer18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" 4 xonali son kiriting = ");
+        float a = scanner.nextInt();
+        int b = (int) (a/1000);
+        System.out.println(" Mingliklar xonasidagi son " + b);
+    }
+}
